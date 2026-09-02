2831b21ef0693dfccfc5787e061baa3a6102dcc4
S4NKALP <sankalptharu50028@gmail.com>
2026-09-03 00:41:01 +0545

    feat(examples): route app and admin URLs

---
+++ examples/example_project/urls.py
@@ -0,0 +1,9 @@
+"""Root URL configuration for the example project."""
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("payments.urls")),
+]
